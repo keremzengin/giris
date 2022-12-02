4a76a6422bbc93a36a4ef146ab88b5dd03af350a
Kerem <Kerem@KRM>
2022-12-02 17:48:17 +0300

    Add project files.

---
+++ HelloWold/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] regions = new string[3, 3]
+            {
+                {"istanbul", "izmit", "balıkesir"},
+                {"ankara", "konya", "kırıkkale"},
+                {"izmir", "muğla", "manisa"},
+            };
+            for (int i = 1; i<=regions.GetUpperBound )
+
+
+        }
+
+
+    }
+}
